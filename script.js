b1db1b40cf7969bbff669920578faca8955ce55f
Ijair Delani <ijair.blasterdown@gmail.com>
2026-09-21 22:05:42 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function nomePortfolio() {
+    return "Ijair Delani";
+}
+
+function tituloPortfolio() {
+    return "Ijair Delani - Portfólio";
+}
+
+function tecnologias() {
+    return ["Java", "JavaScript", "Python", "HTML e CSS"];
+}
+
+function anoAtual() {
+    return 2026;
+}
+
+function quantidadeTecnologias() {
+    return tecnologias().length;
+}
+
+module.exports = {
+    nomePortfolio,
+    tituloPortfolio,
+    tecnologias,
+    anoAtual,
+    quantidadeTecnologias
+};
