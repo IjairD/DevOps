9d8d91e04f2d687fabf395c6f7e6572284dcc71c
Ijair Delani <ijair.blasterdown@gmail.com>
2026-09-21 22:07:28 -0300

    Create script.test.js

---
+++ script.test.js
@@ -0,0 +1,27 @@
+const {
+    nomePortfolio,
+    tituloPortfolio,
+    tecnologias,
+    anoAtual,
+    quantidadeTecnologias
+} = require("./script");
+
+test("deve retornar o nome correto do portfólio", () => {
+    expect(nomePortfolio()).toBe("Ijair Delani");
+});
+
+test("deve retornar o título correto da página", () => {
+    expect(tituloPortfolio()).toBe("Ijair Delani - Portfólio");
+});
+
+test("deve conter Java na lista de tecnologias", () => {
+    expect(tecnologias()).toContain("Java");
+});
+
+test("deve conter JavaScript na lista de tecnologias", () => {
+    expect(tecnologias()).toContain("JavaScript");
+});
+
+test("deve possuir quatro tecnologias cadastradas", () => {
+    expect(quantidadeTecnologias()).toBe(4);
+});
